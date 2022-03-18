589984801da37a4b6a72b33e42d2c7bbb052306f
Patrick Boyle <Patrick_Boyle1@baylor.edu>
2022-03-17 19:40:54 -0500

    Update Frontend

--- frontend/src/components/UserComponent.js
+++ frontend/src/components/UserComponent.js
@@ -1,55 +1,119 @@
 import React from 'react';
 import UserService from '../services/UserService';
+import axios from 'axios';
 
 class UserComponent extends React.Component {
+    accounts = [];
+    constructor(props) {
+        super(props);
+        this.state = {username: '', password: ''};
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleChangeStatus = this.handleChangeStatus.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-    constructor(props){
-        super(props)
-        this.state = {
-            users:[]
-        }
+    handleChangeStatus(event) {
+        this.setState({level: event.target.value});
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
     }
 
     componentDidMount(){
         UserService.getUsers().then((response) => {
-            this.setState({ users: response.data})
+            this.accounts = response.data;
         });
     }
 
-    render (){
+
+    handleSubmit(event) {
+
+        const userDto = {
+            password: this.state.password,
+            username: this.state.username,
+        };
+
+        axios.post("http://localhost:80/user/users", userDto)
+            .then(res => {
+                console.log('Account added successfully!');
+                console.log(res);
+                this.setState( {
+                    username: res.data.username,
+                    password: res.data.password
+                });
+                alert("Welcome " + this.state.username + " " + this.state.password);
+            })
+
+        event.preventDefault();
+    }
+
+
+
+    render() {
         return (
             <div>
+                <h1>Create Account</h1>
+
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label>
+                            Username:
+                        </label>
+                        <input name = "username" className="form-control" value={this.state.username} type="text" onChange={this.handleInputChange} />
+
+                    </div>
+
+                    <div>
+                        <label>
+                            Password:
+                        </label>
+                        <input name = "password"  value={this.state.password} type="text" onChange={this.handleInputChange} />
+
+                    </div>
+
+                    <input type="submit" value="Submit" />
+                </form>
+                <br />
+                <label>{this.state.message}</label>
+
                 <h1 className = "text-center"> Users List</h1>
                 <table className = "table table-striped">
                     <thead>
-                        <tr>
+                    <tr>
 
-                            <td> User Id</td>
-                            <td> User First Name</td>
-                            <td> User Last Name</td>
-                            <td> User Email Id</td>
-                        </tr>
+                        <td> User Id</td>
+                        <td> User Username</td>
+                        <td> User Password</td>
+                    </tr>
 
                     </thead>
                     <tbody>
-                        {
-                            this.state.users.map(
-                                user => 
+                    {
+                        this.accounts.map(
+                            user =>
                                 <tr key = {user.id}>
-                                     <td> {user.id}</td>   
-                                     <td> {user.firstName}</td>   
-                                     <td> {user.lastName}</td>   
-                                     <td> {user.email}</td>   
+                                    <td> {user.id}</td>
+                                    <td> {user.username}</td>
+                                    <td> {user.password}</td>
                                 </tr>
-                            )
-                        }
+                        )
+                    }
                     </tbody>
                 </table>
 
-            </div>
 
-        )
+            </div>
+        );
     }
+
+
 }
 
 export default UserComponent
