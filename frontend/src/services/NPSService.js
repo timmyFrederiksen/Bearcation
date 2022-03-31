5127af6f1f9c440d5a2a995f7196f29076a32820
FrancisBoyle11 <91507837+FrancisBoyle11@users.noreply.github.com>
2022-03-31 03:09:07 -0500

    added activities list

--- frontend/src/services/NPSService.js
+++ frontend/src/services/NPSService.js
@@ -4,7 +4,7 @@ const api_key = 'api_key=os9gdUMtfjWN14L4zo5a33m7m4beCQAC1xrZAjh9';
 
 class NPSService {
     getParkInfo(){
-        return axios.get(NPS_base_url + "parks?limit=25&" + api_key);
+        return axios.get(NPS_base_url + "parks?limit=500&" + api_key);
     }
 }
 
