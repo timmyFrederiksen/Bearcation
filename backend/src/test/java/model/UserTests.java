57f969774112fc5d99ebf82b9255b1913217d5f3
timmyFrederiksen <timmyfrederiksen@gmail.com>
2022-04-08 21:37:49 -0500

    test update
    This might work but I'm not 100% if this is what the test does as intended?

--- backend/src/test/java/model/UserTests.java
+++ backend/src/test/java/model/UserTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.*;
 import com.example.userGuide.model.*;
 import org.mockito.*;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -14,6 +15,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = UserController.class)
@@ -33,12 +36,26 @@ public class UserTests {
 //            "{\"name\":\"Spring\",\"description\":\"10Steps\",\"steps\":[\"Learn
 //    Maven\",\"Import Project\",\"First Example\",\"Second Example\"]}";
 // Do we need this Json stuff either???
+    String exampleUserJson = "{\"id\":\"6942069\",\"password\":\"password\",\"username\":\"test123\"}";
 
     @Test
     public void retrieveUser() throws Exception {
 
         Mockito.when(usercontroller.UserById(6942069L)).thenReturn(user);
 
-        
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
+                "/user/6942069").accept(
+                MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+        System.out.println(result.getResponse());
+        String expected = "{id:6942069,password:password,username:test123}";
+
+        JSONAssert.assertEquals(expected, result.getResponse()
+                .getContentAsString(), false);
+
     }
+
+
 }
