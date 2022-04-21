10759658f43b22dd6a838dc63aaaf260584b3e67
FrancisBoyle11 <91507837+FrancisBoyle11@users.noreply.github.com>
2022-04-21 00:45:34 -0500

    fixed controllers that were previously deleted by poison/accident commit

--- backend/src/main/java/bearcation/controller/ReviewController.java
+++ backend/src/main/java/bearcation/controller/ReviewController.java
@@ -1,29 +1,25 @@
 package bearcation.controller;
 
-import bearcation.model.Location;
-import bearcation.model.User;
-import bearcation.service.ReviewService;
-import bearcation.model.Review;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/review")
 public class ReviewController {
-    @Autowired
-    private ReviewService reviewService;
-
-    @GetMapping("/reviews")
-    public List<Review> getReviews() {
-        return reviewService.getReviews();
-    }
-
-    @PostMapping("/addReview")
-    public Review saveReview(@RequestBody Review review){
-        return reviewService.createReview(review);
-    }
 
 }
+
+//    @Autowired
+//    private ReviewService reviewService;
+//
+//    @GetMapping("/reviews")
+//    public List<Review> getReviews() {
+//        return reviewService.getReviews();
+//    }
+//
+//    @PostMapping("/addReview")
+//    public Review saveReview(@RequestBody Review review){
+//        return reviewService.createReview(review);
+//    }
