aa4f08d32bb129a3c3fd84a062c8c937c52a3aea
Joshua McKone <56418466+ArcturusGrimm@users.noreply.github.com>
2022-04-28 19:40:53 -0500

    Removing mistaken commits
    Old commit changes from my branch were brought over into main mistakenly.

--- backend/src/main/java/bearcation/BearcationApplication.java
+++ backend/src/main/java/bearcation/BearcationApplication.java
@@ -1,43 +1,13 @@
 package bearcation;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-import bearcation.model.Location;
-import bearcation.model.Review;
-import bearcation.model.User;
-import bearcation.repository.LocationRepository;
-import bearcation.repository.ReviewRepository;
-import bearcation.repository.UserRepository;
-
 @SpringBootApplication
-public class BearcationApplication implements CommandLineRunner {
+public class BearcationApplication {
   public static void main(String[] args){
     SpringApplication.run(BearcationApplication.class, args);
-  }
-
-  @Autowired
-  private UserRepository userRepository;
-
-  @Autowired
-  private LocationRepository locationRepository;
-
-  @Autowired
-  private ReviewRepository reviewRepository;
-
-  @Override
-  public void run(String...args) throws Exception{
-    this.instantiateDatabase();
-  }
-
-  private void instantiateDatabase(){
-    User userTest = new User("testUser", "testPassword");
-    Location testLocation = new Location(userTest, "locationName", "testAddress", "testDetails");
-    this.userRepository.save(userTest);
-    this.locationRepository.save(testLocation);
-    this.reviewRepository.save(new Review(5.0, "testDescription", userTest, testLocation));
   }
 }
